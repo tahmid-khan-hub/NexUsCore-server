f49e7bdefab608313ed3f912f5a1af05ab4b0ac4
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-11 01:22:51 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = 3000
@@ -40,6 +40,13 @@ async function run() {
 
     app.get('/courses', async(req, res) =>{
       const result = await CoursesCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.delete('/courses/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await CoursesCollection.deleteOne(query);
       res.send(result);
     })
 
