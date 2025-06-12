ded4a92ad701675aaf18a55a8cb292cbce29a710
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-12 18:18:01 +0600

    added delete method for UserCoursesCollection

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
       res.json({ enrolled: !!result }); 
     })
 
+    app.delete('/userCourses/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await UsersEnrolledCourses.deleteOne(query);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
